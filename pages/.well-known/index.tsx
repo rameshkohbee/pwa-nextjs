67793f9b6bcfa290c9330cfae4626d6dabb86a8b
Mayank Prasad <mayank@Mayanks-MacBook-Pro-2.local>
2022-11-25 18:51:16 +0530

    added assetslink file

---
+++ pages/.well-known/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function WellKnown() {
+
+    return <div>
+        <h1>Well Known</h1>
+    </div>
+}
---
+++ pages/products/index.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+
+
+export default function Products() {
+    return <div><h1>Porducts page</h1></div>
+}
